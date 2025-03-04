70e01b71b467fc1f72885521de5a62f4867c6db6
henri <125201813+lebonhenri@users.noreply.github.com>
2025-03-04 10:34:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template, request, flash
 
+app = Flask(__name__)
+app.secret_key = "mango_is-sweet"
+@app.route("/hello")
+
+def index():
+    flash("Quel est ton nom?")
+    return render_template("index.html")
+    
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("Hello " + str(request.form['name_input']) + ", heureux de te revoir")
+    return render_template("index.html")
